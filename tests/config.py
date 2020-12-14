256bb4bada7fe3e0d7fc5cb3727ad861d6a36b58
skasberger <mail@stefankasberger.at>
2020-12-14 23:28:07 +0100

    Fix Bug: browser env variable loads as list

--- tests/config.py
+++ tests/config.py
@@ -6,12 +6,15 @@ class Config(BaseSettings):
     INSTANCE: str
     USER_AGENT: str = None
     HEADLESS: bool = True
-    BROWSER: str = None
+    BROWSER: list = None
     WINDOW_HEIGHT: int = 1400
     WINDOW_WIDTH: int = 1400
     USER_NORMAL: str = None
     USER_NORMAL_NAME: str = None
     USER_NORMAL_PWD: str = None
+    USER_SHIBBOLETH: str = None
+    USER_SHIBBOLETH_NAME: str = None
+    USER_SHIBBOLETH_PWD: str = None
     TEST_CONFIG_FILENAME: str = "test-config.json"
     FILENAME_DATAVERSES: str = "dataverses.json"
     FILENAME_DATASETS: str = "datasets.json"
